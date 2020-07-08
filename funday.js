9d9a6ae2b62d1b1582fc20b3fa5727874fdf22ad
mufraswid <fauzanrafisidiq@gmail.com>
2020-07-08 15:40:48 +0700

    Add images for markers

--- funday.js
+++ funday.js
@@ -22,14 +22,14 @@ function initialize() {
   // Add Locations HERE (Can also add icons on each marker, to be added)
   // [Marker's caption, lattitude, longitude, icon location]
   var locations = [
-    ['<div style="width: 170px;">POS SADIKIN</div>', -6.87939, 107.61272],
-	['<div style="width: 170px;">POS BESKEM</div>', -6.87333, 107.61254],
-	['<div style="width: 170px;">POS MEKDI DAGO</div>', -6.88497, 107.61344],
-	['<div style="width: 170px;">POS CIWALK</div>', -6.89324, 107.60546],
-	['<div style="width: 170px;">POS GASIBU</div>', -6.89902, 107.61701],
-	['<div style="width: 170px;">POS BORMA</div>', -6.87713, 107.6181],
-	['<div style="width: 170px;">POS MONJU</div>', -6.89343, 107.61855],
-	['<div style="width: 170px;">POS BALTOS</div>', -6.89908, 107.60817]
+    ['<div style="width: 170px;">POS SADIKIN</div>', -6.87939, 107.61272, 'img/didit.png'],
+	['<div style="width: 170px;">POS BESKEM</div>', -6.87333, 107.61254, 'img/kahimsejken.png'],
+	['<div style="width: 170px;">POS MEKDI DAGO</div>', -6.88497, 107.61344, 'img/almyra.png'],
+	['<div style="width: 170px;">POS CIWALK</div>', -6.89324, 107.60546, 'img/rojap.png'],
+	['<div style="width: 170px;">POS GASIBU</div>', -6.89902, 107.61701, 'img/naradita.png'],
+	['<div style="width: 170px;">POS BORMA</div>', -6.87713, 107.6181, 'img/ridwan.png'],
+	['<div style="width: 170px;">POS MONJU</div>', -6.89343, 107.61855, 'img/farabi.png'],
+	['<div style="width: 170px;">POS BALTOS</div>', -6.89908, 107.60817, 'img/juro.png']
   ];
 
 
@@ -39,8 +39,8 @@ function initialize() {
       map: pano,
       visible: false,
       zIndex: 999,
-      zoomControl: false
-	  //icon: locations[i][3]
+      zoomControl: false,
+	  icon: locations[i][3]
     });
 
     google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
