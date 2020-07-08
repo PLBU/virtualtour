b51240570af09223678928eab0999f3ea5170719
mufraswid <fauzanrafisidiq@gmail.com>
2020-07-08 21:58:34 +0700

    Revise locations

--- funday.js
+++ funday.js
@@ -22,14 +22,15 @@ function initialize() {
   // Add Locations HERE (Can also add icons on each marker, to be added)
   // [Marker's caption, lattitude, longitude, icon location]
   var locations = [
-    ['<div style="width: 170px;">POS SADIKIN</div>', -6.87939, 107.61272, 'img/didit.png'],
-	['<div style="width: 170px;">POS BESKEM</div>', -6.87333, 107.61254, 'img/kahimsejken.png'],
-	['<div style="width: 170px;">POS MEKDI DAGO</div>', -6.88497, 107.61344, 'img/almyra.png'],
-	['<div style="width: 170px;">POS CIWALK</div>', -6.89324, 107.60546, 'img/rojap.png'],
-	['<div style="width: 170px;">POS GASIBU</div>', -6.89902, 107.61701, 'img/naradita.png'],
-	['<div style="width: 170px;">POS BORMA</div>', -6.87713, 107.6181, 'img/ridwan.png'],
-	['<div style="width: 170px;">POS MONJU</div>', -6.89343, 107.61855, 'img/farabi.png'],
-	['<div style="width: 170px;">POS BALTOS</div>', -6.89908, 107.60817, 'img/juro.png']
+    ['<div style="width: 170px;">POS SADIKIN</div>', -6.87939, 107.61272, 'img/didit.png'],			
+	['<div style="width: 170px;">POS JEPRUT</div>', -6.889113, 107.608057, 'img/kahimsejken.png'], 
+	['<div style="width: 170px;">POS MEKDI DAGO</div>', -6.88497, 107.61344, 'img/almyra.png'],	//
+	['<div style="width: 170px;">POS SPBU PERTAMINA</div>', -6.890169, 107.613247, 'img/rojap.png'], // 
+	['<div style="width: 170px;">POS TAMAN FILM</div>', -6.898686, 107.607746, 'img/naradita.png'], 
+	['<div style="width: 170px;">POS BAKSIL</div>', -6.885179, 107.610161, 'img/ridwan.png'], 
+	['<div style="width: 170px;">POS MONJU</div>', -6.89343, 107.61855, 'img/farabi.png'], 
+	['<div style="width: 170px;">POS UPNORMAL DU</div>', -6.898178, 107.613641, 'img/juro.png'],
+	['<div style="width: 170px;">POS BALTOS</div>', -6.898794, 107.609274, 'img/garda.png']
   ];
 
 
