03fc1ec82be675d55179121f44699769270583f2
mufraswid <fauzanrafisidiq@gmail.com>
2020-07-09 16:46:48 +0700

    Adding meeting links

--- funday.js
+++ funday.js
@@ -22,15 +22,15 @@ function initialize() {
   // Add Locations HERE (Can also add icons on each marker, to be added)
   // [Marker's caption, lattitude, longitude, icon location]
   var locations = [
-    ['<div style="width: 170px;">POS SADIKIN</div>', -6.87939, 107.61272, 'img/didit.png'],			
-	['<div style="width: 170px;">POS JEPRUT</div>', -6.888931, 107.608089, 'img/kahimsekjen.png'], 
-	['<div style="width: 170px;">POS MEKDI DAGO</div>', -6.88497, 107.61344, 'img/almyra.png'],	//
-	['<div style="width: 170px;">POS SPBU PERTAMINA</div>', -6.890169, 107.613247, 'img/rojap.png'], // 
-	['<div style="width: 170px;">POS TAMAN FILM</div>', -6.898686, 107.607746, 'img/naradita.png'], 
-	['<div style="width: 170px;">POS BAKSIL</div>', -6.885179, 107.610161, 'img/ridwan.png'], 
-	['<div style="width: 170px;">POS MONJU</div>', -6.89343, 107.61855, 'img/farabi.png'], 
-	['<div style="width: 170px;">POS UPNORMAL DU</div>', -6.898178, 107.613641, 'img/juro.png'],
-	['<div style="width: 170px;">POS BALTOS</div>', -6.898794, 107.609274, 'img/garda.png']
+    ['<div style="width: 170px;">https://meet.google.com/hxe-cwzp-ycn</div>', -6.87939, 107.61272, 'img/didit.png'],			
+	['<div style="width: 170px;">https://meet.google.com/suj-aywh-jis</div>', -6.888931, 107.608089, 'img/kahimsekjen.png'], 
+	['<div style="width: 170px;">https://meet.google.com/nme-ftmh-aye</div>', -6.88497, 107.61344, 'img/almyra.png'],	
+	['<div style="width: 170px;">https://meet.google.com/hxe-cwzp-ycn</div>', -6.890169, 107.613247, 'img/rojap.png'], 
+	['<div style="width: 170px;">https://meet.google.com/vjp-sbhx-buk</div>', -6.898686, 107.607746, 'img/naradita.png'], 
+	['<div style="width: 170px;">https://meet.google.com/taa-druk-dak</div>', -6.885179, 107.610161, 'img/ridwan.png'], 
+	['<div style="width: 170px;">https://meet.google.com/kga-vaff-ekj</div>', -6.89343, 107.61855, 'img/farabi.png'], 
+	['<div style="width: 170px;">https://meet.google.com/dne-bfov-fht</div>', -6.898178, 107.613641, 'img/juro.png'],
+	['<div style="width: 170px;">https://meet.google.com/rej-gujz-eep</div>', -6.898794, 107.609274, 'img/garda.png']
   ];
 
 
