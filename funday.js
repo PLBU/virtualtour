01fc9bb5edbdfb65b07a48157caa5f51e434c1ae
mufraswid <fauzanrafisidiq@gmail.com>
2020-07-21 20:42:21 +0700

    Update portals and new locations

--- funday.js
+++ funday.js
@@ -22,15 +22,41 @@ function initialize() {
   // Add Locations HERE (Can also add icons on each marker, to be added)
   // [Marker's caption, lattitude, longitude, icon location]
   var locations = [
-    ["<div class='qung'>https://meet.google.com/hxe-cwzp-ycn</div>", -6.87939, 107.61272, 'img/daWRpdA.png'],			
-	['<div style="width: 170px;">https://meet.google.com/suj-aywh-jis</div>', -6.888931, 107.608089, 'img/kYWhpbXNla2plbg.png'], 
-	['<div style="width: 170px;">https://meet.google.com/nme-ftmh-aye</div>', -6.88497, 107.61344, 'img/abG15cmE.png'],	
-	['<button onclick="portalTest()">EKSTERNAL GOES TO ITALY</button>', -6.893224, 107.610439, 'img/roamFw.png'], 
-	['<div style="width: 170px;">https://meet.google.com/vjp-sbhx-buk</div>', -6.898686, 107.607746, 'img/nYXJhZGl0YQ.png'], 
-	['<div style="width: 170px;">https://meet.google.com/taa-druk-dak</div>', -6.885179, 107.610161, 'img/riZHdhbg.png'], 
-	['<div style="width: 170px;">https://meet.google.com/kga-vaff-ekj</div>', -6.89343, 107.61855, 'img/fYXJhYmk.png'], 
-	['<div style="width: 170px;">https://meet.google.com/dne-bfov-fht</div>', -6.898178, 107.613641, 'img/jdXJv.png'],
-	['<button onclick="goBack()">GO BACK!</button>', 43.722828, 10.392177, 'img/gYXJkYQ.png']
+	// Paris
+	['<button onclick="toParis()">EKSTERNAL GOES TO FRANCE</button><br><b>Ini merupakan keterangan</b>', -6.87939, 107.61272], //sadikin
+	['<button onclick="toParis()">EKSTERNAL GOES TO FRANCE</button><br><b>Ini merupakan keterangan</b>', -6.89343, 107.61855], //monju
+	['<div style="width: 170px;">POS MENARA EIFFEL</div>', 48.857931, 2.295467],
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', 48.855454, 2.301249],
+	
+	// Italy
+	['<button onclick="toPisa()">EKSTERNAL GOES TO ITALY</button><br><b>Ini merupakan keterangan</b>', -6.888931, 107.608089], //jeprut
+	['<button onclick="toPisa()">EKSTERNAL GOES TO ITALY</button><br><b>Ini merupakan keterangan</b>', -6.898178, 107.613641], //upnormal
+	['<div style="width: 170px;">POS MENARA PISA</div>', 43.722897, 10.396467], 
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', 43.7215745, 10.3971504],
+	
+	// Jogja
+	['<button onclick="toJogja()">EKSTERNAL GOES TO JOGJA</button><br><b>Ini merupakan keterangan</b>', -6.88497, 107.61344], //mcd dago
+	['<button onclick="toJogja()">EKSTERNAL GOES TO JOGJA</button><br><b>Ini merupakan keterangan</b>', -6.898794, 107.609274], //baltos 
+	['<div style="width: 170px;">POS BOROBUDUR</div>', -7.607521, 110.203681], 
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', -7.606929, 110.202715],
+	
+	// Jakarta
+	['<button onclick="toJakarta()">EKSTERNAL GOES TO JAKARTA</button><br><b>Ini merupakan keterangan</b>', -6.890169, 107.613247], //spbu
+	['<button onclick="toJakarta()">EKSTERNAL GOES TO JAKARTA</button><br><b>Ini merupakan keterangan</b>', -6.876435, 107.611753], //angcis
+	['<div style="width: 170px;">POS MONAS</div>', -6.173288, 106.827659],
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', -6.171659, 106.826946],
+	
+	// Rio
+	['<button onclick="toRio()">EKSTERNAL GOES TO RIO</button><br><b>Ini merupakan keterangan</b>', -6.898686, 107.607746], //taman film
+	['<button onclick="toRio()">EKSTERNAL GOES TO RIO</button><br><b>Ini merupakan keterangan</b>', -6.879173, 107.620036], //kanayakan
+	['<div style="width: 170px;">POS JESUS</div>', -22.951827, -43.210497],
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', -22.951940, -43.210898],
+	
+	// Kuala Lumpur
+	['<button onclick="toKualaLumpur()">EKSTERNAL GOES TO KUALA LUMPUR</button><br><b>Ini merupakan keterangan</b>', -6.885179, 107.610161], //baksil
+	['<button onclick="toKualaLumpur()">EKSTERNAL GOES TO KUALA LUMPUR</button><br><b>Ini merupakan keterangan</b>', -6.878426, 107.609397], //sangkuriang 	
+	['<div style="width: 170px;">POS PETRONAS</div>', 3.158228, 101.711308],
+	['<button onclick="goBack()">BACK TO BANDUNG</button>', 3.158845, 101.709261]
   ];
 
 
@@ -41,7 +67,6 @@ function initialize() {
       visible: false,
       zIndex: 999,
       zoomControl: false,
-	  icon: locations[i][3]
     });
 
     google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
@@ -61,12 +86,33 @@ function initialize() {
   pano.setPano('L4WNkSLF-mB52zujd2oR6g');
 }
 
-function portalTest() {
-	pano.setPano("_tfTouCq7SDErkInK11UnA");
-}
 
 function goBack() {
 	pano.setPano("L4WNkSLF-mB52zujd2oR6g");
+}
+
+function toParis() {
+	pano.setPano("sTl4iz8yrMDvwZgi6aeFgw");
+}
+
+function toPisa() {
+	pano.setPano("B2MSxoluho_KlmONMS4zCA");
+}
+
+function toJogja() {
+	pano.setPano("CyRjGnaBGjIvXiMFP-4jig");
+}
+
+function toJakarta() {
+	pano.setPano("r18eyTnCRT90PGcqsy-hnQ");
+}
+
+function toRio() {
+	pano.setPano("PrOCBIKiwp0_AW0ULdmqJA");
+}
+
+function toKualaLumpur() {
+	pano.setPano("l5GdBeZBIys3jKbp4ijn4w");
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
